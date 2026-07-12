b66c34d78650ed768f6ebc1c8293708972ce6bbd
Aditya Shiwarkar <adityabshiwarkar9960@gmail.com>
2026-07-12 18:20:13 +0530

    Update local startup settings

--- app.py
+++ app.py
@@ -2535,5 +2535,4 @@ bootstrap()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get("FLASK_DEBUG", "1") == "1"
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(host="0.0.0.0", port=port)  #change
